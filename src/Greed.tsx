50328f78b453872ab7b6492fb3d997cf8a6a1ae3
sumit45doc <ss8559283@gmail.com>
2021-09-13 17:52:13 +0530

    type props

---
+++ src/Greed.tsx
@@ -0,0 +1,10 @@
+type GreedProps = {
+    language: string
+}
+export const Greed = (props: GreedProps) => {
+    return (
+        <div>
+            Helo {props.language} , Sorry Js            
+        </div>
+    )
+}
